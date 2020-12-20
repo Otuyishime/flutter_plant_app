6e50b9e23fd7c84e39abedeaf6adbb711f1cb074
Otuyishime <oliviertyishime@gmail.com>
2020-12-20 15:55:29 -0600

    Added default color palette and default padding
    Adde default constants:
    Color palette and default padding

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// Color palette to use in our app
+const pPrimaryColor = Color(0xFF0C9869);
+const pTextColor = Color(0xFF3C4046);
+const pBackgroundColor = Color(0xFFF9F8FD);
+
+const double pDefaultPadding = 20.0;
